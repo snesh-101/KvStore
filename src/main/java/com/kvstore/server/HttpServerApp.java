f4d7bd4727d7cf9ccf1c5cad64d57600a0430d5a
Sneha <bhardwajsneha111@gmail.com>
2026-05-05 00:34:52 +0530

    add root html

--- src/main/java/com/kvstore/server/HttpServerApp.java
+++ src/main/java/com/kvstore/server/HttpServerApp.java
@@ -24,6 +24,33 @@ public class HttpServerApp {
         server.createContext("/del", new DeleteHandler(engine));
 
         server.setExecutor(Executors.newFixedThreadPool(10));
+        server.createContext("/", exchange -> {
+            String response = """
+                    <html>
+                    <body>
+                    <h2>KV Store API Running</h2>
+
+                    <h3>Endpoints:</h3>
+
+                    <b>SET:</b><br>
+                    <code>/set?key=a&value=10</code><br><br>
+
+                    <b>GET:</b><br>
+                    <code>/get?key=a</code><br><br>
+
+                    <b>DELETE:</b><br>
+                    <code>/del?key=a</code><br><br>
+
+                    </body>
+                    </html>
+                    """;
+
+            byte[] responseBytes = response.getBytes();
+            exchange.getResponseHeaders().add("Content-Type", "text/html");
+            exchange.sendResponseHeaders(200, responseBytes.length);
+            exchange.getResponseBody().write(responseBytes);
+            exchange.getResponseBody().close();
+        });
         server.start();
 
         System.out.println("Server started on port " + port);
